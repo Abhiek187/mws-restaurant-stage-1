3bdc9aee911701a514d28a66383bc657969b5e61
Abhiek187 <achaudhuri2011@yahoo.com>
2018-06-29 22:11:39 -0400

    feat: Enable mapbox

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYWJoaWVrMTg3IiwiYSI6ImNqajBxZ3c2dDBrcGozdnQ0bnNocHc0Mm4ifQ.uQeBUolbO1hdyRPtsEG04Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
